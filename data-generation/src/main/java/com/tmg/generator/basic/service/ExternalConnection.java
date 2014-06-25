ff5e72616f2c309d4d8d3056f19e8a22219ec9dd
govindrgaikwad <govindrgaikwad@gmail.com>
2014-06-25 18:18:09 +0530

    Data Generation Tool

---
+++ data-generation/src/main/java/com/tmg/generator/basic/service/ExternalConnection.java
@@ -0,0 +1,34 @@
+package com.tmg.generator.basic.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tmg.generator.basic.exception.DataAccessException;
+
+@Service("ExternalConnection")
+public class ExternalConnection {
+
+	private static Logger logger = Logger.getLogger(ExternalConnection.class);
+
+	@Autowired
+	private DataSource dataSource;
+
+	public Connection getConnection() throws DataAccessException {
+		Connection conn = null;
+		try {
+			conn = dataSource.getConnection();
+		} catch (SQLException e) {
+			logger.error("Error creating connection" + e.getMessage(), e);
+			throw new DataAccessException("Error creating connection"
+					+ e.getMessage(), e);
+		}
+		return conn;
+	}
+
+}
